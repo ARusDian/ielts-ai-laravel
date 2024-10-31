e58f60c90fac010f5b201c5ab35eb727c7e42993
yudha87 <muhammad.yudha87@gmail.com>
2024-10-31 11:33:04 +0800

    sss

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,11 +1,9 @@
 import "../css/app.css";
 import "./bootstrap";
-
 import React from "react";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot } from "react-dom/client";
-
 import 'regenerator-runtime';
 
 
