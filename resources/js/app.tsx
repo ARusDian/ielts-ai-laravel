698a6d4c436aabca5d87f768b834d4318b461d0f
yudha87 <muhammad.yudha87@gmail.com>
2024-10-31 11:33:04 +0800

    sss

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -7,9 +7,6 @@ import { createRoot } from "react-dom/client";
 import 'regenerator-runtime';
 
 
-import 'regenerator-runtime';
-
-
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
